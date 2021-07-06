27ebca38f6d5bb52df5c507127166e6282281b94
Hari Ravula <hari4dev@gmail.com>
2021-07-06 17:14:02 +0530

    A new beginning.

--- src/components/bio.js
+++ src/components/bio.js
@@ -46,7 +46,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          Follow him
+          You should follow him:
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
             @hariravula
